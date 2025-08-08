daff8b3386c6fb9ad40efe9cfad54a2491cd9dc7
Houssam-Eddine Mney <houssameddine.mney@aircanada.ca>
2025-08-08 12:12:32 +0100

    Initial Commit

---
+++ Radio Thmanyah/Radio_ThmanyahApp.swift
@@ -0,0 +1,17 @@
+//
+//  Radio_ThmanyahApp.swift
+//  Radio Thmanyah
+//
+//  Created by Houssam-Eddine Mney on 8/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct Radio_ThmanyahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Radio ThmanyahTests/Radio_ThmanyahTests.swift
@@ -0,0 +1,16 @@
+//
+//  Radio_ThmanyahTests.swift
+//  Radio ThmanyahTests
+//
+//  Created by Houssam-Eddine Mney on 8/8/2025.
+//
+
+import Testing
+
+struct Radio_ThmanyahTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
